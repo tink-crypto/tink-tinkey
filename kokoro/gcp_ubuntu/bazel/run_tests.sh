352fcadfc2d45b74ad681a80fb95cf743f0de520
Moreno Ambrosin <ambrosin@google.com>
2022-10-25 04:40:58 -0700

    Use injected `use_bazel.sh` script when running on Kokoro
    PiperOrigin-RevId: 483637527
    Change-Id: I839bf7a976541a908bffde790db9a28bf11893fe

--- kokoro/gcp_ubuntu/bazel/run_tests.sh
+++ kokoro/gcp_ubuntu/bazel/run_tests.sh
@@ -26,7 +26,8 @@ if [[ -n "${KOKORO_ROOT:-}" ]]; then
   # folder in KOKORO_ARTIFACTS_DIR that matches git* will make the test fail.
   TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
   cd "${TINK_BASE_DIR}/tink_tinkey"
-  use_bazel.sh "$(cat .bazelversion)"
+  chmod +x "${KOKORO_GFILE_DIR}/use_bazel.sh"
+  "${KOKORO_GFILE_DIR}/use_bazel.sh" "$(cat .bazelversion)"
 fi
 
 : "${TINK_BASE_DIR:=$(cd .. && pwd)}"
