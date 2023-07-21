bfa8b2812cac3188c2ffbe88b4a9cd6bfb2f268d
Thomas Holenstein <tholenst@google.com>
2023-07-21 07:21:50 -0700

    Upgrade the Tinkey homebrew formula to the next Tinkey version.
    PiperOrigin-RevId: 549940348
    Change-Id: If2d9e8b72a6eaf0210dbdb60d717299990efa1e8

--- tinkey.rb
+++ tinkey.rb
@@ -6,8 +6,8 @@
 class Tinkey < Formula
   desc "A command line tool to generate and manipulate keysets for the Tink cryptography library"
   homepage "https://github.com/tink-crypto/tink-tinkey"
-  url "https://storage.googleapis.com/tinkey/tinkey-1.8.0.tar.gz"
-  sha256 "ee262e3705837366188920687a35a224468380f58f02916ca0567d1e4b66cdcf"
+  url "https://storage.googleapis.com/tinkey/tinkey-1.9.0.tar.gz"
+  sha256 "fd00a2f7839e135224860b4d40069918f356ace3965224576c9336d4daecd374"
 
   def install
     bin.install "tinkey"
