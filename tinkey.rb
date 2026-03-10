8281766c7b0064bd8e206f709bb734a3070c441e
Thomas Holenstein <tholenst@google.com>
2026-03-10 06:08:15 -0700

    Fix the hash for Tinkey.
    I validated on my laptop -- in fact I still have the file I downloaded on 2026-02-05 which also has this SHA256. Hence, I'm not sure what I did here.
    
    https://github.com/tink-crypto/tink-tinkey/issues/7
    
    PiperOrigin-RevId: 881386921
    Change-Id: I02ba60c4bd722b48fec49cf8bd2c5ef0370d270d

--- tinkey.rb
+++ tinkey.rb
@@ -7,7 +7,7 @@ class Tinkey < Formula
   desc "A command line tool to generate and manipulate keysets for the Tink cryptography library"
   homepage "https://github.com/tink-crypto/tink-tinkey"
   url "https://storage.googleapis.com/tinkey/tinkey-1.13.0.tar.gz"
-  sha256 "293010ccc0adcbb51a19ca51bb67f6aea8a849ac8c6dabc2544087d79122226f"
+  sha256 "690eea87a059e05284134749391499df10df7b420a676480bac5cf0968fa6d20"
 
   def install
     bin.install "tinkey"
